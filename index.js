a6d035d0b1fdb3d30600e8efcdf3ade5adbe9a0b
juanDev-47 <juanp.arenas@udea.edu.co>
2021-10-11 07:39:48 -0500

    se configura la whitelist en cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 // const mongoose = require('mongoose');
 require('./config/db');
-require('dotenv').config();
+require('dotenv').config({path: 'variables.env'});
 
 // CORS permite que un cliente se conecte a otro servidor para el intercambio de recursos
 const cors = require('cors');
@@ -15,8 +15,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// definir un dominio para recibir las peticiones
+const whitelist = [process.env.FRONTEND_URL];
+const corsOption = {
+    origin: (origin, callback) => {
+        // console.log(origin);
+        // revisar si la peticion viene de un servidor en whitelist
+        const existe = whitelist.some(dominio => dominio === origin);
+        if(existe) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}
+
 // Habilitar CORS
-app.use(cors());
+app.use(cors(corsOption));
 
 // rutas de la app
 app.use('/', routes());
